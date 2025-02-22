2dc5cea951f5d60f3cdada9fa8110608dee6c463
İsmail Kırcı <46162287+krcstudio59@users.noreply.github.com>
2025-02-22 10:24:17 +0300

    text helper eklendi

---
+++ lib/features/ui/helper/text_helper.dart
@@ -0,0 +1,40 @@
+class UITextHelper {
+  static final String name = "Name";
+  //Login View Texts
+  static final String email = "E-Posta";
+  static final String username = "Kullanıcı Adı";
+  static final String password = "Şifre";
+  static final String login = "Oturum Aç";
+  static final String forgetPassword = "Şifremi Un,uttum";
+  static final String register = "Kayıt Ol";
+  static final String welcome = "Hoşgeldiniz";
+  static final String start = "Başlamak için lütfen giriş yapınız.";
+}
+
+class LoginTextHelper {
+  static final String email = "E-Posta";
+  static final String username = "Kullanıcı Adı";
+  static final String password = "Şifre";
+  static final String login = "Oturum Aç";
+  static final String forgetPassword = "Şifremi Unuttum";
+  static final String register = "Kayıt Ol";
+  static final String welcome = "Hoşgeldiniz";
+  static final String start = "Başlamak için lütfen giriş yapınız.";
+}
+
+class RegisterTextHelper {
+  static final String email = "E-Posta";
+  static final String username = "Kullanıcı Adı";
+  static final String password = "Şifre";
+  static final String register = "Kayıt Ol";
+  static final String welcome = "Hoşgeldiniz";
+  static final String start = "Başlamak için lütfen giriş yapınız.";
+}
+
+class TermsTextHelper {
+  static final String termsTitle = "Kullanıcı Sözleşmesi";
+  static final String terms = "Kullanıcı Sözleşmesi";
+  static final String privacy = "Gizlilik Politikası";
+  static final String termsText = "Kullanıcı Sözleşmesi metni buraya gelecek.";
+  static final String privacyText = "Gizlilik Politikası metni buraya gelecek.";
+}
